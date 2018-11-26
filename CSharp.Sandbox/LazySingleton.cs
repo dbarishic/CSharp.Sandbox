94781fb05f82b9f765fe33fe977c6118b71acaba
Davor Barishic-Jaman <cro.davor@outlook.com>
2018-11-26 01:51:59 +0100

    Singleton implementations.

---
+++ CSharp.Sandbox/LazySingleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp.Sandbox
+{
+    public sealed class LazySingleton
+    {
+        private static readonly Lazy<LazySingleton> lazy =
+            new Lazy<LazySingleton>(() => new LazySingleton());
+
+        public static LazySingleton Instance { get { return lazy.Value; } }
+
+        private LazySingleton()
+        {
+        }
+    }
+}
---
+++ CSharp.Sandbox/ThreadSafeSingletonNoLocks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Sandbox
+{
+    public sealed class ThreadSafeSingletonNoLocks
+    {
+        private static readonly ThreadSafeSingletonNoLocks instance = new ThreadSafeSingletonNoLocks();
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+
+        //If marked BeforeFieldInit then the type's initializer method is executed at, or sometime before, first access to any static field defined for that type
+
+        //If not marked BeforeFieldInit then that type's initializer method is executed at (i.e., is triggered by):
+            //1. first access to any static or instance field of that type, or
+            //2. first invocation of any static, instance or virtual method of that type
+
+        static ThreadSafeSingletonNoLocks()
+        {
+        }
+
+        private ThreadSafeSingletonNoLocks()
+        {
+        }
+
+        public static ThreadSafeSingletonNoLocks Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
